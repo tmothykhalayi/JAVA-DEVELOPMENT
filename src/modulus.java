fa8c6fa08c54eccc72b74daf6b6463d75d4abed3
tmothy khalayi <nandwatimothykhalayi@GMAIL.COM>
2025-02-03 18:51:27 +0300

    update

---
+++ src/modulus.java
@@ -0,0 +1,9 @@
+public class modulus {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            if (i%2 != 0) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }}
