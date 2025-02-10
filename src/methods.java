cc176d4d1bf56623c86cae757659cd33a522daf2
tmothy khalayi <nandwatimothykhalayi@GMAIL.COM>
2025-02-10 08:18:48 +0300

    update

---
+++ src/methods.java
@@ -0,0 +1,34 @@
+public class methods {
+    public static void main(String[] args){
+
+            // method =  a block of reusable code that is executed when called ()
+
+            happyBirthday("Timothy", 30);
+        }
+        static void happyBirthday(String name, int age){
+            System.out.println("Happy Birthday to you!");
+            System.out.printf("Happy Birthday dear %s!\n", name);
+            System.out.printf("You are %d years old!\n", age);
+            System.out.println("Happy Birthday to you!\n");
+        }
+        static double square(double number){
+            return number * number;
+        }
+        static double cube(double number){
+            return number * number * number;
+        }
+        static String getFullName(String first, String last){
+            return first + " " + last;
+        }
+        static boolean ageCheck(int age){
+            if(age >= 18){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    }
+
+
+
